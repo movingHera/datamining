ca59b5d05f06779d6b2bcf9d5c963ed858e74efb
taizilongxu <468137306@qq.com>
2014-10-17 18:42:36 +0800

    重构

--- apriori.py
+++ apriori.py
@@ -4,17 +4,17 @@
 class Apriori(object):
 
     def __init__(self, filename):
-        self.min_support = 14
+        self.min_support = 15
         self.min_confidence = 20
         self.item_num = 11 # 项目数
 
         self.location = [[i] for i in range(self.item_num)]
-        self.support = [0] * len(self.location)
-        self.num = [i for i in range(self.item_num)] # 记录item
+        self.support = self.sut(self.location)
+        self.num = list(sorted(set([j for i in self.location for j in i])))# 记录item
 
-        self.pre_support = self.sut(self.location)
+        self.pre_support = self.support
         self.pre_location = self.location
-        self.pre_num = list(sorted(set([j for i in self.pre_location for j in i])))
+        self.pre_num = self.num
 
         self.item_name = [] # 项目名
         self.find_item_name(filename)
@@ -52,7 +52,7 @@ class Apriori(object):
                         support[index_num] += 1
         return support
 
-    def select(self,  c):
+    def select(self, c):
         "返回位置"
         stack = []
         for i in self.pre_location:
@@ -65,16 +65,17 @@ class Apriori(object):
         # 多重列表去重
         import itertools
         s = sorted([sorted(i) for i in stack])
-        self.location = list(s for s,_ in itertools.groupby(s))
+        location = list(s for s,_ in itertools.groupby(s))
+        return location
 
-    def del_location(self):
+    def del_location(self, support, location):
         "清除不满足条件的候选集"
         # 小于最小支持度的剔除
-        for index,i in enumerate(self.support):
+        for index,i in enumerate(support):
             if i < 1000 * self.min_support / 100:
-                self.support[index] = 0
+                support[index] = 0
         # apriori第二条规则,剔除
-        for index,j in enumerate(self.location):
+        for index,j in enumerate(location):
             sub_location = [j[:index_loc] + j[index_loc+1:]for index_loc in range(len(j))]
             flag = 0
             for k in sub_location:
@@ -82,43 +83,48 @@ class Apriori(object):
                     flag = 1
                     break
             if flag:
-                self.support[index] = 0
+                support[index] = 0
         # 删除没用的位置
-        self.location = [i for i,j in zip(self.location,self.support) if j != 0]
-        self.support = [i for i in self.support if i != 0]
+        location = [i for i,j in zip(location,support) if j != 0]
+        support = [i for i in support if i != 0]
+        return support, location
 
     def loop(self):
         s = 2
-        while self.num and self.location:
+        while True:
             print '-'*80
             print 'The' ,s - 1,'loop'
-            print 'location' , self.pre_location
-            print 'support' , self.pre_support
-            print 'num' , self.pre_num
+            print 'location' , self.location
+            print 'support' , self.support
+            print 'num' , self.num
             print '-'*80
 
             # 生成下一级候选集
-            self.select(s)
-            self.support = self.sut(self.location)
-            self.del_location()
+            location = self.select(s)
+            support = self.sut(location)
+            support, location = self.del_location(support, location)
             s += 1
-            if not self.location:
-                break
+            if  location and support:
+                self.pre_num = list(sorted(set([j for i in self.location for j in i])))
+                self.pre_location = self.location
+                self.pre_support = self.support
 
-            self.pre_num = list(sorted(set([j for i in self.location for j in i])))
-            self.pre_location = self.location
-            self.pre_support = self.support
+                self.num = list(sorted(set([j for i in location for j in i])))
+                self.location = location
+                self.support = support
+            else:
+                break
 
     def confidenc_sup(self):
         "计算confidence"
-        if sum(self.pre_support) == 0:
-            print 'min_support error'
+        if sum(self.support) == 0:
+            print 'first min_support error'
         else:
-            for index_location,each_location in enumerate(self.pre_location):
+            for index_location,each_location in enumerate(self.location):
                 del_num = [each_location[:index] + each_location[index+1:] for index in range(len(each_location))]
                 xy = self.sut(del_num)
                 print del_num
-                print self.pre_support[index_location]
+                print self.support[index_location]
                 print xy
                 if not del_num[0]:
                     print 'min_support error'
